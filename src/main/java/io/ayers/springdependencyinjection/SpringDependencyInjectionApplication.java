0c0939679a6a8b63297b9f2d2417950588233721
Zach Ayers <ayers.dev@icloud.com>
2020-12-14 14:33:09 -0600

    feat: Initial Project Create

---
+++ src/main/java/io/ayers/springdependencyinjection/SpringDependencyInjectionApplication.java
@@ -0,0 +1,13 @@
+package io.ayers.springdependencyinjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDependencyInjectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDependencyInjectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/ayers/springdependencyinjection/SpringDependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ayers.springdependencyinjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDependencyInjectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
